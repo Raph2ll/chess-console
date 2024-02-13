74b5b1e0e1e94aaadb0a4869862dbc5b698fb79f
Raph2ll <raphael.ph2ll@gmail.com>
2024-02-13 16:39:02 -0300

    feat: Position

---
+++ board/Position.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chess_console.board
+{
+    public class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Posicao(int line, int column) {
+            Line = line;
+            Column = column;
+        }
+
+        public void SetValues(int line, int column) {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString() {
+            return $"{Line} , {Column}"
+        }
+    }
+}
