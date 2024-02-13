073cc5ea2164a1fed203879c329000afeeafee96
Raph2ll <raphael.ph2ll@gmail.com>
2024-02-13 16:39:11 -0300

    feat: enum Colors

---
+++ board/enum/Colors.cs
@@ -0,0 +1,13 @@
+namespace chess_console.board.enum
+{
+    public enum Colors
+    {
+        White,
+        Black,
+        Yellow,
+        Blue,
+        Red,
+        Green,
+        Orange
+    }
+}
