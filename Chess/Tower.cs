0c4eae9d5d4133793b550288baaf8609e5c65ec9
Raph2ll <raphael.ph2ll@gmail.com>
2024-02-28 14:14:16 -0300

    feat: tower moves

--- Chess/Tower.cs
+++ Chess/Tower.cs
@@ -8,7 +8,67 @@ namespace chess_console.Chess
     public class Tower : Piece
     {
         public Tower(Colors color, BoardService board) : base(color, board) { }
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.Piece(position);
+            return piece == null || piece.Color != Color;
+        }
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] mat = new bool[Board.Lines, Board.Columns];
 
+            Position position = new Position(0, 0);
+
+            // up
+            position.SetValues(position.Line - 1, position.Column);
+            while (Board.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Column] = true;
+                if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Line = position.Line - 1;
+            }
+            return mat;
+            // down
+            position.SetValues(position.Line + 1, position.Column);
+            while (Board.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Column] = true;
+                if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Line = position.Line + 1;
+            }
+            return mat;
+            // right
+            position.SetValues(position.Line, position.Column + 1);
+            while (Board.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Column] = true;
+                if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Column = position.Column + 1;
+            }
+            return mat;
+            // left
+            position.SetValues(position.Line, position.Column - 1);
+            while (Board.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Column] = true;
+                if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Column = position.Column - 1;
+            }
+            return mat;
+
+        }
         public override string ToString()
         {
             return "T";
